50f8fb0e244157c8e82cae499c4c1ada71c2ff80
Greg Rell <rell.greg@gmail.com>
2023-11-05 22:18:11 -0500

    temperature conversion challenge

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 
+
 namespace one{
     std::string name = "Ass";
 }
@@ -13,34 +14,32 @@ typedef std::string mystring_t;
 using myotherstring_t = std::string; // this is preferred instead of typedefs because of templates
 
 int main(){
+    system("clear");
+    char unit;
+    double inTemp;
+    double convertedTemp;
 
-    double a;
-    double b;
-    char op;
-    double result;
-    bool err=false;
+    std::cout << "*****  Temp conversion *****\n";
+    std::cout << "Enter the unit to convert from:";    
+    std::cin >> unit;
+    std::cout << "Enter the temperature:\n";    
+    std::cin >> inTemp;
 
-    std::cout << "Enter first number:";
-    std::cin >> a;
-    std::cout << "Enter operator:";
-    std::cin >> op;
-    std::cout << "Enter second number:";
-    std::cin >> b;
-    
-    std::cout << "Evaluating " << a << op << b << "\n\n";
-
-    switch(op){
-        case '+':
-            result = a+b;
-            break;
-        case '-':
-            result = a-b;
-            break;
-        default:
-            std::cout << "Not Valid Operation\n";
-            err=true;
+    if(unit == 'f' || unit == 'F'){
+        //convert F to C
+        convertedTemp = (inTemp-32)/1.8;
+        std::cout << inTemp << "F = " << convertedTemp << " C\n";
     }
-    
-    err ? std::cout << "Erorr" : std::cout << "Result:  " << result;
+    else if(unit =='c'|| unit=='C'){
+        //convert C to F
+        convertedTemp = (1.8 * inTemp) + 32;
+        std::cout << inTemp << "C = " << convertedTemp << " F\n";
+
+    }
+
+
+    std::cout << "****************************";
+
+   
     return 0;
 }
